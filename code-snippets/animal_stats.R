e0533f486336a31b35c86ef9fd2ffbb58c24c61a
sinkovit <sinkovit@sdsc.edu>
2022-05-27 14:59:42 -0700

    Modified animal_stats() function to use rounding pixel sizes to calculate grid dimensions. This turns things around from previous version where grid dimension was set and then used to determine pixel size.

--- code-snippets/animal_stats.R
+++ code-snippets/animal_stats.R
@@ -7,26 +7,45 @@
 # equidistance projection and t is time in minutes relative to the
 # first observation in the entire data set
 
-# Note that I'm using the R range() function for both performance and
+# Note 1 - I'm using the R range() function for both performance and
 # to maintain more compact code. It returns a vector of min and max
 # values
+
+# Note 2 - code is written assuming that data has already been
+# loaded. The animal_attribute() function is first defined and call to
+# function is made at bottom of file
 
 # --------------------------------------------------------------------
 
 animal_attributes <- function(data_df) {
 
+  # Replicate C printf functionality
   printf <- function(...) cat(sprintf(...))
+
+  # Custom rounding to 2 significant digits if x > 10, 1 digit otherwise
+  custom_round <- function(x) {return (signif(x, min(2,floor(log10(x)))))}
 
   animals <- append(as.list(sort(unique(data_df$local_identifier))), "all")
   max_pixels <- c(50, 100, 200, 500)
 
+  printf("\n")
+  printf("The following table gives the extent of animal movement for the\n")
+  printf("individuals and for the data set as a whole, along with the grid\n")
+  printf("dimensions resulting from various pixel sizes. Keep in mind that larger\n")
+  printf("grids result in longer calculations, so you may want to choose a pixel\n")
+  printf("size that result in a smaller grid for preliminary calculations.\n")
+  printf("\n")
+  printf("E-W(m) and N-S(m) are the east-west and north-south ranges, in meters\n")
+  printf("px(m) is the pixel size in meters and grid is the resulting grid dimensions\n")
+  printf("\n")
+
   printf("%7s ", "id")
-  printf("%9s %9s %9s %9s %10s %10s ", "long_min", "long_max", "lat_min", "lat_max", "x_range", "y_range")
+  printf("%9s %9s %9s %9s %10s %10s ", "long_min", "long_max", "lat_min", "lat_max", "E-W(m)  ", "N-S(m)  ")
   for (max_pix in max_pixels) {
-    printf("%7s %7s ", "dims", "size")
+    printf("%6s %7s ", "px(m)", "grid")
   }
-  printf("\n------- --------- --------- --------- --------- ---------- ---------- ------- ------- ")
-  printf("------- ------- ------- ------- ------- -------\n")
+  printf("\n------- --------- --------- --------- --------- ---------- ---------- ------ ------- ")
+  printf("------ ------- ------ ------- ------ -------\n")
 
   for (local_id in animals) {
     if(local_id == "all") {
@@ -53,17 +72,24 @@ animal_attributes <- function(data_df) {
          nx <- max_pix
          ny <- as.integer(nx * (y_range/x_range))
          cell.sz <- x_range/nx
+	 cell.sz <- custom_round(cell.sz)
+	 nx <- as.integer(x_range/cell.sz)
+	 ny <- as.integer(y_range/cell.sz)
       } else {
          ny <- max_pix
          nx <- as.integer(ny * (x_range/y_range))
          cell.sz <- y_range/ny
+	 cell.sz <- custom_round(cell.sz)
+	 nx <- as.integer(x_range/cell.sz)
+	 ny <- as.integer(y_range/cell.sz)
       }
       dims <- sprintf("%dx%d", nx, ny)
-      printf("%7s %7.2f ", dims, cell.sz)
+      printf("%6.1f %7s ", cell.sz, dims)
     }
     printf("\n")
   }
 }
 
+# Calling the function; assumes data_df is already defined
 animal_attributes(data_df)
 
