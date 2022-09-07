b5283349cd7d52f3dd72e674e8ec323321635869
Mona Wong <mona@sdsc.edu>
2022-09-06 18:50:46 -0700

    removed unused code and added Bob calculateRaster2D() (issue #12)

--- src/shiny/plotDataframe.R
+++ src/shiny/plotDataframe.R
@@ -31,27 +31,51 @@
 # OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
 # MODIFICATIONS.
 
+calculateRaster2D <- function(gpsdata, sig2obs, t.max, cell.sz,
+                              xmin, xmax, ymin, ymax) {
 
-# Read GPS movement data from file
-GPSDataLoader <- function(sig2obs, tmax, cell.sz, path.file) {
-  panda <- read.table(path.file, header=TRUE)
-  
-  xmin <- min(panda$x)
-  ymin <- min(panda$y)
-  xmax <- max(panda$x)
-  ymax <- max(panda$y)
-  xrange <- xmax-xmin
-  yrange <- ymax-ymin
-  nx = xrange/cell.sz
-  ny = yrange/cell.sz
-  mv.dat <- initializeMovementData(panda$time, panda$x, panda$y, sig2obs=sig2obs,
-                                   t.max=tmax)
-  mkde.obj <- initializeMKDE2D(xmin, cell.sz, nx, ymin, cell.sz, ny)
-  dens.res <- initializeDensity(mkde.obj, mv.dat)
-  
-  mkde.obj <- dens.res$mkde.obj
-  mv.dat <- dens.res$move.dat
-  return ( mkde.obj )
+# Calculate rasters for each indivdual in a dataframe using mkde
+# package and return the results as a list of rasters
+#
+# gpsdata - data frame containing animal movement data
+# sig2obs - location error variance
+# t.max - maximum time between locations
+# cell.sz - cell size
+# xmin, xmax - min/max x coordinate
+# ymin, ymax - min/max y coordinate
+
+  library(mkde)
+  library(raster)
+
+  # Initialize list of rasters
+  rasters <- list()
+
+  for (id in unique(gpsdata$id)) {
+      x <- gpsdata[which(gpsdata$id == id), "xdata"]
+      y <- gpsdata[which(gpsdata$id == id), "ydata"]
+      t <- gpsdata[which(gpsdata$id == id), "time"]
+
+      xrange <- xmax - xmin
+      yrange <- ymax - ymin
+
+      nx <- as.integer(xrange/cell.sz)
+      ny <- as.integer(yrange/cell.sz)
+
+      print(paste("identifier =", id))
+      print("Home range dimensions (pixels/voxels)")
+      print(paste("nx =", nx, "ny =", ny))
+      print("Home range dimensions (meters)")
+      print(paste("xrange =", xrange, "yrange =", yrange))
+      print(paste("cell size =", cell.sz))
+      print("---------------------------------------")
+
+      Sys.sleep(3)
+
+      mv.dat <- initializeMovementData(t, x, y, sig2obs=25.0, t.max=185.0)
+      mkde.obj <- initializeMKDE2D(xmin, cell.sz, nx, ymin, cell.sz, ny)
+      dens.res <- initializeDensity(mkde.obj, mv.dat)
+      mkde.obj <- dens.res$mkde.obj
+      rasters <- append(rasters, list(mkde.obj))
+   }
+   return(rasters)
 }
-
-
