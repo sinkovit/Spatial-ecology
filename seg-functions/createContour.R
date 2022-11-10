4704c173f83b15f5b9a6bac6664c34de3ddcae27
Robert Sinkovits <sinkovit@sdsc.edu>
2022-11-10 15:36:07 -0800

    Add files via upload

---
+++ seg-functions/createContour.R
@@ -0,0 +1,50 @@
+createContours <- function(mkde2d.obj, probs, basename, all=TRUE) {
+
+   # Input
+   # mkde2d.obj: MKDE object
+   # probs: list of probabilities for image contours
+   # basename: base file name for raster and shape files - basename_[all|outer]contour.[asc|dbf|shp|shx]
+   # all: if true, use all probability contours, if false use only outer contour
+   #
+   # Output
+   # Completion code (0 for success)
+   #
+   # Usage examples
+   # createContours(rasters[[1]], probs, "condor", all=TRUE)
+   # createContours(mkde2d.obj, probs, "tejon-pig", all=FALSE)
+
+   library(rgdal)
+   library(rgeos)
+
+   # Create raster from MKDE object
+   rst.mkde = mkdeToRaster(mkde2d.obj) 
+
+   # Sort prob contours into ascending order and remove values <= 0
+   probs <- sort(probs[probs > 0])
+   probs_max <- tail(probs, n=1)
+
+   # Set filenames and contour probabilites based on whether all
+   # contours or outer contour are used
+   if (all) {
+      filename <- paste(basename, "_allcontour", sep="")
+      contour_probs <- probs
+   } else {
+      filename <- paste(basename, "_outercontour", sep="")
+      contour_probs <- tail(probs, n=1)
+   }
+   
+   # Create/display/write shapefiles
+   cont <- computeContourValues(mkde2d.obj, prob = contour_probs)
+   rst.cont = cut(rst.mkde, breaks = c(cont$threshold, max(values(rst.mkde), na.rm = T)))
+   plot(rst.cont)
+   contour_display <- contour(rst.mkde, add = T, levels = cont$threshold, lwd = 2.0)
+   raster.contour <- rasterToContour(rst.mkde, levels = cont$threshold)
+   writeOGR(obj = raster.contour, dsn=".", layer = filename, driver = "ESRI Shapefile", overwrite = T)
+
+   # Create/write raster of outer contour
+   raster.contour = spChFIDs(raster.contour, paste(contour_probs, "% Contour Line", sep=""))
+   writeRaster(rst.cont, filename, format = "ascii", overwrite = T)
+   writeOGR(obj = raster.contour, dsn=".", layer = filename, driver = "ESRI Shapefile", overwrite = T)
+
+   return(0)
+}
