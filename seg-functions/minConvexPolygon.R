aca1e2f81141d126b7a5a72dabeea06ab13c77f6
Robert Sinkovits <sinkovit@sdsc.edu>
2022-11-28 11:22:18 -0800

    Add files via upload

--- seg-functions/minConvexPolygon.R
+++ seg-functions/minConvexPolygon.R
@@ -1,4 +1,4 @@
-minConvexPolygon <- function(gpsdata, utm.zone) {
+minConvexPolygon <- function(gpsdata, utm.zone, ids) {
 
    # Input
    # gpsdata: Data frame containing animal GPS data
@@ -18,35 +18,45 @@ minConvexPolygon <- function(gpsdata, utm.zone) {
    library(scales)
    library(ggmap) 
 
-   # geom_polygon function needs the animal id to be an integer
+   # convert animal id to string as needed by geom_polygon
    gpsdata$id <- as.character(gpsdata$id)
 
+   # Generate the basemap using all data
    gpsdata.sp <- gpsdata[, c("id", "xdata", "ydata")]
    coordinates(gpsdata.sp) <- c("xdata", "ydata")
    crsstr <- paste("+proj=utm +zone=", utm.zone, " +datum=WGS84 +units=m +no_defs", sep="")
    proj4string(gpsdata.sp) <- CRS(crsstr)
-   gpsdata.mcp <- mcp(gpsdata.sp, percent = 100)
-   #plot(gpsdata.sp, col = as.factor(gpsdata.sp@data$id), pch = 16)
-   #plot(gpsdata.mcp, col = alpha(1:5, 0.5), add = TRUE)
-
    gpsdata.spgeo <- spTransform(gpsdata.sp, CRS("+proj=longlat"))
-   gpsdata.mcpgeo <- spTransform(gpsdata.mcp, CRS("+proj=longlat"))
    mybasemap <- get_stamenmap(bbox = c(left = min(gpsdata.spgeo@coords[,1])-0.005, 
                                     bottom = min(gpsdata.spgeo@coords[,2])-0.005, 
                                     right = max(gpsdata.spgeo@coords[,1])+0.005, 
                                     top = max(gpsdata.spgeo@coords[,2])+0.005), 
                                     zoom = 8)
 
+   # Filter data based on selected animal IDs then plot on basemap
+   ids <- as.character(ids)
+   gpsdata.sp <- gpsdata.sp[gpsdata.sp$id %in% ids, ]
+   gpsdata.mcp <- mcp(gpsdata.sp, percent = 100)
+   gpsdata.spgeo <- spTransform(gpsdata.sp, CRS("+proj=longlat"))
+   gpsdata.mcpgeo <- spTransform(gpsdata.mcp, CRS("+proj=longlat"))
    gpsdata.geo <- data.frame(gpsdata.spgeo@coords, id = gpsdata.spgeo@data$id)
 
-   mymap.hr <- ggmap(mybasemap) + 
-   	    geom_polygon(data = fortify(gpsdata.mcpgeo),  
-	    aes(long, lat, colour = id, fill = id),
-	    alpha = 0.3) + # alpha sets the transparency
-  	    geom_point(data = gpsdata.geo, 
-	    aes(x = xdata, y = ydata, colour = id))  +
-	    theme(legend.position = c(0.15, 0.80)) +
-	    labs(x = "Longitude", y = "Latitude")
+   # Without minimum convex polygon
+   mymap.hr <- ggmap(mybasemap) +
+  	       geom_point(data = gpsdata.geo, 
+	       aes(x = xdata, y = ydata, colour = id), size = 0.8, alpha = 0.5)  +
+	       theme(legend.position = c(-0.2, 0.90)) +
+	       labs(x = "Longitude", y = "Latitude")
 
-   return(mymap.hr)
+   # With minimum convex polygon
+   mymap.mcp.hr <- mymap.hr +
+               geom_polygon(data = fortify(gpsdata.mcpgeo),
+	       aes(long, lat, colour = id, fill = id),
+	       alpha = 0.3) # alpha sets the transparency
+
+   maps <- list()
+   maps <- append(maps, list(mymap.hr))
+   maps <- append(maps, list(mymap.mcp.hr))
+
+   return(maps)
 }
